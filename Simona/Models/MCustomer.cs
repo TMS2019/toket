c559dda302f16e4cf1c6900190bd93b1c98b5318
agustiana perdana <perdanarobert@gmail.com>
2020-09-17 11:31:52 +0700

    dfdffdsfdsf

---
+++ Simona/Models/MCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simona.Models
+{
+    public class MCustomer
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+
+
+    }
+}
